1d3995a5f7336e21cb272ac08ac038c26237e0fa
Michael Wong <michael.w228@gmail.com>
2021-05-17 13:20:57 -0400

    moving platforms

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
 const container = document.querySelector('.container')
 const doodler = document.createElement('div')
 let doodlerLeftSpace = 50
-let doodlerbottomSpace = 150
+let doodlerbottomSpace = 250
 let gameOver = false
 let platformCount = 5
-let platform = [] 
+let platforms = []
 
-class Platform{
-    constructor(newPlatBottom){
+class Platform {
+    constructor(newPlatBottom) {
         this.bottom = newPlatBottom
-        this.left = Math.random()* 315
+        this.left = Math.random() * 315
         this.visual = document.createElement('div')
 
         const visual = this.visual
         visual.classList.add('platform')
-        visual.style.left = this.left+ 'px'
-        visual.style.bottom = this.bottom+ 'px'
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom + 'px'
         container.appendChild(visual)
 
     }
 }
 
-function createDoodler(){
+function createDoodler() {
     container.appendChild(doodler)
     doodler.classList.add('doodler')
     doodler.style.left = doodlerLeftSpace + 'px'
     doodler.style.bottom = doodlerbottomSpace + 'px'
 }
 
-function createPlatforms(){
-    for(let i=0; i<platformCount; i++){
-        let platGap = 600/platformCount
-        let newPlatBottom = 100+i*platGap
+function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+        let platGap = 600 / platformCount
+        let newPlatBottom = 100 + i * platGap
         let newPlatform = new Platform(newPlatBottom)
-        platform.push(newPlatform)
-        console.log(platform)
+        platforms.push(newPlatform)
+        console.log(platforms)
     }
 }
 
-function start(){
-    if(!gameOver){
+function movePlatforms() {
+    if (doodlerbottomSpace > 200) {
+        platforms.forEach(platform => {
+            platform.bottom -= 4
+            let visual = platform.visual
+            visual.style.bottom = platform.bottom + 'px'
+        })
+    }
+}
+
+function start() {
+    if (!gameOver) {
         createPlatforms()
         createDoodler()
+        setInterval(movePlatforms, 30)
     }
 }
 
